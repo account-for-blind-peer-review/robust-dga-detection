35fd91fe2c4f055b03c96656f2e8f2b1050e4d69
account-for-blind-peer-review <153219474+account-for-blind-peer-review@users.noreply.github.com>
2024-03-26 12:18:46 +0100

    REPOSITORY MOVED

--- src/robust_dga_detection/defenses/__init__.py
+++
@@ -1,9 +0,0 @@
-from robust_dga_detection.defenses.discrete_domain_batches import (
-    DiscreteDomainbatchAttack,
-    NLPAttackCatalogue,
-    EmbeddingSpaceDiscretizationCatalogue,
-)
-from robust_dga_detection.defenses.embedding_space_batches import (
-    EmbeddingSpaceAttackCatalogue,
-    EmbeddingSpaceBatchAttack,
-)
--- src/robust_dga_detection/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from robust_dga_detection.models.binary_dga_detector_with_character_embedding import (
-    BinaryDgaDetectorWithCharacterEmbedding,
-)
-from robust_dga_detection.models.cnn_resnet import CNNResNetWithEmbedding
